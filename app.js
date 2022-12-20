cb8b12ae50b2c45c39a97fdf5abdaa6141638ee4
JoaoVTeixeira <joao14665@gmail.com>
2022-12-20 14:00:38 -0400

    nova versao

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
 // Importa e instancia um servidor express
 var express = require("express"), 
-       app = express();
+app = express();
 
-var http = require('http');
-var fs = require('fs');
-
-// Configurações futuras
-
-
-
-// Define a pasta onde irão ficar as views
-app.set("views", __dirname + "/views");
 // Define o template engine usado nas views
 app.set("view engine", "ejs");
+// Define a pasta onde irão ficar as views
+app.set("views","./views");
 
 // Define a pasta public para conteúdo estático
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
-// Importa o módulo express-load
-var express = require("express"),    
-    load = require("express-load"),
-    app = express();
 
-// Carrega todas as scripts da pasta controller e routes
-load("controllers")    
-    .then("routes")
-    .into(app);
-
-    // Sobe o servidor HTTP na posta 3000
-app.listen(3000, function () {
-    console.log("Servidor está no ar.");
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-module.exports = function (app) {
-        var home = app.controllers.home;
-        app.get("/", home.index);
-        app.post("/entrar", home.entrar);
-            };
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+});
 
+
+app.listen(8080, function () {
+    console.log("Servidor está no ar.");
+});
 
